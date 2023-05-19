f75e2e6f89299d22fa2d8778bdf37adc39ee317e
yellhtetaung <yairhtet248.yha@gmail.com>
2023-05-19 18:19:59 +0630

    create first static web page

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const fs = require("fs");
 
 http
   .createServer((req, res) => {
-    fs.readFile("./page/index.html", (error, data) => {
-      res.write(data);
-      res.end();
-    });
+    const url = req.url;
+
+    if (url == "/") {
+      fs.readFile("./page/index.html", (error, data) => {
+        if (error) throw error;
+        res.write(data);
+        res.end();
+      });
+    } else if (url == "/about") {
+      fs.readFile("./page/about.html", (error, data) => {
+        if (error) throw error;
+        res.write(data);
+        res.end();
+      });
+    } else if (url == "/contact") {
+      fs.readFile("./page/contact.html", (error, data) => {
+        if (error) throw error;
+        res.write(data);
+        res.end();
+      });
+    }
   })
   .listen(4000, () => {
     console.log("Server is running on port 4000");
