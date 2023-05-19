2d5097e6c367f0964cb95bd93acde3ed66ae3c62
yellhtetaung <yairhtet248.yha@gmail.com>
2023-05-19 17:55:43 +0630

    first module

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const fs = require("fs");
-
-console.log("start program");
-
-// const note = fs.readFileSync("./node.txt");
-// console.log(note.toString());
-
-fs.readFile("./node.txt", (error, data) => {
-  if (error) throw error;
-  console.log(data.toString());
-});
-
-console.log("end program");
+const helper = require("./helper");
+console.log(helper.sum(1, 2));
+console.log(helper.isLikeBlack);
