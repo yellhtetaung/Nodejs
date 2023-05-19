ef1ac56846c3f0632952c760d048305bfd12a770
yellhtetaung <yairhtet248.yha@gmail.com>
2023-05-19 18:11:03 +0630

    render html document

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require("http");
+const fs = require("fs");
 
 http
   .createServer((req, res) => {
-    res.end("Hello Nodjs");
+    fs.readFile("./page/index.html", (error, data) => {
+      res.write(data);
+      res.end();
+    });
   })
   .listen(4000, () => {
     console.log("Server is running on port 4000");
