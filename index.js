68049c7ed563db4385bb6a841feb3c124a8da5b5
yellhtetaung <yairhtet248.yha@gmail.com>
2023-05-19 18:02:25 +0630

    create own http server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const helper = require("./helper");
-console.log(helper.sum(1, 2));
-console.log(helper.isLikeBlack);
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.end("Hello Nodjs");
+  })
+  .listen(4000, () => {
+    console.log("Server is running on port 4000");
+  });
